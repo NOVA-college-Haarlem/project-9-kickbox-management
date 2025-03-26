b0e2e8da0dafed4953e288483aae9203c2ee0618
Lars Mooij <mooij.lars2005@gmail.com>
2025-03-26 11:34:20 +0100

    het maken van de routes voor training ind e web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TrainerController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::name("training.")->group(function(){
+    
+    Route::prefix("training")->group(function(){        
+        Route::get('/',                 [TrainerController::class, 'index'])->name('index');
+    });
 });
