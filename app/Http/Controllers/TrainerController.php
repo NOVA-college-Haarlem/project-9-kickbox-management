a3cb1b30b47f65dd1b30922301a21937de656c50
Lars Mooij <mooij.lars2005@gmail.com>
2025-03-26 11:33:32 +0100

    het aanbrengen van de trainercontorler index view

--- app/Http/Controllers/TrainerController.php
+++ app/Http/Controllers/TrainerController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Trainer;
 
 class TrainerController extends Controller
 {
-    //
+    public function index()
+    {
+        $trainingen = Trainer::all();
+        return view('training.index', compact('training'));
+    }
 }
