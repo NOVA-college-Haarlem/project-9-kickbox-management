eab31e39d83a09f6e6c08a52b3797bc6ee106bbd
Lars Mooij <mooij.lars2005@gmail.com>
2025-03-28 11:19:46 +0100

    maken van de routes met de group

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TrainerController;
+use App\Http\Controllers\TrainingController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,6 +16,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware(['training'])->group(function () {
+    Route::get('/', [TrainingController::class, 'index'])->name('trainings.index');
+    Route::get('/create', [TrainingController::class, 'create'])->name('trainings.create');
+    Route::post('/', [TrainingController::class, 'store'])->name('trainings.store');
+    Route::get('/{id}', [TrainingController::class, 'show'])->name('trainings.show');
 });
 
 require __DIR__.'/auth.php';
