4835c0fe40fc511976c3d2bcbaebcb42d0549a02
Lars Mooij <mooij.lars2005@gmail.com>
2025-03-28 11:19:14 +0100

    het aannmaken van een  moddel middleware controller

--- app/Http/Controllers/TrainingController.php
+++ app/Http/Controllers/TrainingController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Training;
 
 class TrainingController extends Controller
 {
-    //
+    public function index()
+    {
+        $trainingen = Training::all();
+        return view('trainings.index', compact('training'));
+    }
 }
--- app/Models/Training.php
+++ app/Models/Training.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Training extends Model
 {
-    //
+    protected $table = 'trainings';
+    protected $fillable = [
+        'name', 'description', 'start_date', 'end_date', 'user_id', 'difficulty', 'capacity', 'is_recurring'
+    ];
 }
