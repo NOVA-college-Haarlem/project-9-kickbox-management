89bf78cbb27a5d01b2b1109bdd8abbd434bfa524
Luca Wijsman <186515@novacollege.nl>
2025-04-02 10:44:47 +0200

    edited css and index

--- resources/views/training/index.blade.php
+++ resources/views/training/index.blade.php
@@ -1,20 +1,33 @@
-<div class="container">
-    <br>
-        <table class="table table-bordered" style="width: 100%; margin: 0 auto;">
+<x-base-layout title="Training Index">
+    <div class="container">
+        <h2>Training Sessions</h2>
+        <p>Explore all upcoming training sessions and their details below:</p>
+        <br>
+        <table class="training-table">
             <thead>
-                <tr> 
+                <tr>
                     <th>ID</th>
-                    <th>Titel</th>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th>Start Date</th>
+                    <th>End Date</th>
+                    <th>Instructor</th>
+                    <th>Capacity</th>
                 </tr>
             </thead>
-
             <tbody>
                 @foreach ($trainings as $training)
                     <tr>
                         <td>{{ $training->id }}</td>
                         <td>{{ $training->name }}</td>
-                    <tr>
+                        <td>{{ $training->description }}</td>
+                        <td>{{ \Carbon\Carbon::parse($training->start_date)->format('d M Y, H:i') }}</td>
+                        <td>{{ \Carbon\Carbon::parse($training->end_date)->format('d M Y, H:i') }}</td>
+                        <td>{{ $training->instructor->name ?? 'N/A' }}</td>
+                        <td>{{ $training->capacity }}</td>
+                    </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
+</x-base-layout>
