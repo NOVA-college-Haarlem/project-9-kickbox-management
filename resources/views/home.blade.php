3aaec11764af8eaef78bc5c1a1ac58d4d0404e36
Luca Wijsman <186515@novacollege.nl>
2025-04-08 13:26:15 +0200

    changeto smaller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<!-- filepath: c:\Users\Luca Wijsman\Herd\kickboks\resources\views\home.blade.php -->
+<x-base-layout title="Welcome to Kickboxing Academy">
+    <main>
+        <h2>About Our Academy</h2>
+        <p>Welcome to the Kickboxing Academy, where we train champions and help you unleash your inner fighter. Explore our training programs, schedule, and more!</p>
+    </main>
+</x-base-layout>
